bef3c59bc1792f15fe58bba342ed8616377c3ce9
JimmyVicente <jimmyvicentel@gmail.com>
2022-09-23 19:03:37 -0500

    update

--- lib/util_channel.dart
+++ lib/util_channel.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/services.dart';
 
-export  'util_biometric.dart';
-export  'util_mac.dart';
+class UtilChannel{
+  static const MethodChannel _channel = MethodChannel('util_channel');
 
-const MethodChannel channel = MethodChannel('util_channel');
+  static Future<bool> get isInvalidateBiometrics async {
+    bool? result = await _channel.invokeMethod('isInvalidateBiometrics');
+    return result ?? false;
+  }
+
+  static Future<void> get setInvalidateBiometrics async {
+    await _channel.invokeMethod('setInvalidateBiometrics');
+  }
+
+
+  static Future<String> macAddress() async {
+    final String macID = await _channel.invokeMethod('getMacAddress');
+    return macID;
+  }
+}
--- lib/util_mac.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-import 'package:util_channel/util_channel.dart';
-
-class UtilMac {
-  static Future<String> macAddress() async {
-    final String macID = await channel.invokeMethod('getMacAddress');
-    return macID;
-  }
-}
